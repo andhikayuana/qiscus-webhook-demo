d1b68b5897ed588757a7a51d77e946b6a7f9507d
Andhika Yuana <andhikayuana@gmail.com>
2017-09-23 19:17:04 +0700

    ingnore log

--- app/PostCommentHandler.php
+++ app/PostCommentHandler.php
@@ -8,7 +8,6 @@ class PostCommentHandler {
     private $qiscusSecretKey;
     private $qiscusComment;
     private $client;
-    private $response;
 
     public function __construnct($baseUrl, $qiscusSecretKey) {
         $this->baseUrl = $baseUrl;
@@ -29,20 +28,60 @@ class PostCommentHandler {
     }
 
     private function request($body = []) {
-        $this->response = $this->client->request('POST', '/api/v2/rest/post_comment', [
+        $this->createHeaders();
+        $this->createClient();
+        $response = $this->client->request('POST', '/api/v2/rest/post_comment', [
             'headers' => $this->headers,
             'json' => $body
         ]);
+
+        return $response;
+    }
+
+    private function getPayloadFromPostComment() {
+        return json_decode(file_get_contents("php://input"), true);
+    }
+
+    public function getQiscusComment() {
+        return $this->qiscusComment;
     }
 
     public function run() {
-        $this->qiscusComment = file_get_contents("php://input");
+        $this->qiscusComment = $this->getPayloadFromPostComment();
 
-        file_put_contents('log-comment.txt', $this->qiscusComment);
+        file_put_contents('log-comment.txt', json_encode($this->getQiscusComment(), JSON_PRETTY_PRINT));
+    }
 
-        $this->createHeaders();
-        $this->createClient();
-        $this->request([
+    public function getQiscusCommentType() {
+        return $this->qiscusComment['type'];
+    }
+
+    public function getQiscusCommentPayload() {
+        return $this->qiscusComment['payload'];
+    }
+
+    public function getQiscusCommentFrom() {
+        return $this->getQiscusCommentPayload()['from'];
+    }
+
+    public function getQiscusCommentRoom() {
+        return $this->getQiscusCommentPayload()['room'];
+    }
+
+    public function getQiscusCommentMessage() {
+        return $this->getQiscusCommentPayload()['message'];
+    }
+
+    public function makeAnswerNotFound() {
+        return $this->request([
+            'sender_email' => 'ismail@domain.com',
+            'room_id' => '37537',
+            'message' => 'Aduh ga tau nih :D'
+        ]);
+    }
+
+    public function makeAnswer() {
+        return $this->request([
             'sender_email' => 'ismail@domain.com',
             'room_id' => '37537',
             'type' => 'buttons',
@@ -72,7 +111,6 @@ class PostCommentHandler {
         
             ]
         ]);
-        
     }
 
     public function answerGetStarted() {
--- index.php
+++ index.php
@@ -8,3 +8,13 @@ const QISCUS_SDK_SECRET = 'd01f3afa94edd84c44057dbc48ae54ea';
 $handler = new PostCommentHandler(BASE_URL, QISCUS_SDK_SECRET);
 
 $handler->run();
+
+if ($handler->getQiscusComment()['type'] == 'post_comment_mobile') {
+    
+    $handler->makeAnswer();
+
+} else {
+
+    $handler->makeAnswerNotFound();
+    
+}
