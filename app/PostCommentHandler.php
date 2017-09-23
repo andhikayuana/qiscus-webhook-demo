a7645b994b970d1d0dd1abe143096b3eece72d46
Andhika Yuana <andhikayuana@gmail.com>
2017-09-23 16:56:06 +0700

    init

---
+++ app/PostCommentHandler.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace App;
+
+class PostCommentHandler {
+
+    public function hello() {
+        return "hello world from post comment handler wkwk";
+    }
+
+
+    public function getStarted() {
+        // todo
+    }
+
+    public function answerSimulation() {
+        // todo
+    }
+
+    public function answerBuy() {
+        // todo
+    }
+
+    public function answerListAgent() {
+        // todo
+    }
+
+    public function answerInputSalary() {
+        // todo
+    }
+
+}
